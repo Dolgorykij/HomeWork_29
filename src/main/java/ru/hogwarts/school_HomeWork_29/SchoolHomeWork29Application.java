31169ed4ed53b36725f3a9bb21ff4eb6a7d1444c
Dolgorykij <dolgo-rykij@mail.ru>
2024-10-21 03:21:45 +0300

    init

---
+++ src/main/java/ru/hogwarts/school_HomeWork_29/SchoolHomeWork29Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school_HomeWork_29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolHomeWork29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolHomeWork29Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/school_HomeWork_29/SchoolHomeWork29ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school_HomeWork_29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolHomeWork29ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
